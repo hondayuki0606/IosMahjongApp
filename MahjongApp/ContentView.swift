785f96e285a5818dd93ff22328e652a70369ceea
本田尚行 <hondayuki0606@yahoo.co.jp>
2021-04-26 20:46:32 +0900

    Initial Commit

---
+++ MahjongApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MahjongApp
+//
+//  Created by 本田尚行 on 2021/04/26.
+//  Copyright © 2021 本田尚行. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
